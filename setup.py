8b2bb91b78ee9d3c087c03299582c3b0e5284ddf
Phill <phill@peelvalley.com.au>
2018-12-17 10:29:34 +1100

    Fix version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='pvs-smtplib',  
-    version='0.1',
+    version='0.0.2',
     author="Peel Valley Softwware",
     author_email="info@peelvalley.com.au",
     description="Custom smtp client wrapper",
